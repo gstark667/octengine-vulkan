76291f5de7635fbb57070cf0f8d510b9a1556695
Gavin Stark <gstark31897@gmail.com>
2018-09-19 22:36:25 -0500

    working out a good player movement script

--- player.lua
+++ player.lua
@@ -3,18 +3,22 @@ require("gameobject")
 require("physics")
 
 function setup(scene, object)
-    physics_init_capsule(scene, object, 10,  0.5, 2)
+    physics_init_capsule(scene, object, 1,  0.5, 1)
     physics_set_angular_factor(object, 0.0)
 
     camera = scene_add_gameobject(scene, "")
     scene_set_camera(scene, camera)
     gameobject_set_parent(camera, object)
-    gameobject_set_transform(camera, 0, 1, 0)
+    gameobject_set_transform(camera, 0, 0.5, 0)
     gameobject_set_integer(object, "camera", camera)
 
-    gameobject_set_number(object, "acceleration", 100)
-    gameobject_set_number(object, "max_velocity", 5)
-    gameobject_set_number(object, "jump_speed", 5)
+    gameobject_set_integer(object, "grounded", 0)
+    gameobject_set_number(object, "ground_acceleration", 5)
+    gameobject_set_number(object, "air_acceleration", 1)
+    gameobject_set_number(object, "friction", 5)
+    gameobject_set_number(object, "max_velocity", 10)
+    gameobject_set_number(object, "jump_speed", 7)
+    gameobject_set_number(object, "gravity", -9.8)
 
     gameobject_set_number(object, "x_axis", 0)
     gameobject_set_number(object, "z_axis", 0)
@@ -23,14 +27,39 @@ function setup(scene, object)
     gameobject_set_number(object, "y_rot", 0)
 end
 
-function dot(ax, ay, bx, by)
-    ta = math.atan2(ay, ax)
-    tb = math.atan2(by, bx)
+function dot(x, y)
+    local sum = 0
+    for i = 1, #x do
+        sum = sum + x[i] * y[i]
+    end
+    return sum
+end
 
-    va = math.sqrt(ax*ax + ay*ay)
-    vb = math.sqrt(bx*bx + by*by)
+function accelerate(x, z, last_x, last_z, accleration, max_velocity, delta)
+    proj_vel = dot(x, z, last_x, last_z)
+    accel_vel = acceleration * delta
 
-    return va * va * math.cos(math.abs(ta -tb))
+    if proj_vel + accel_vel > max_velocity then
+        accel_vel = max_velocity - proj_vel
+    end
+
+    accel_dir = math.atan2(x, z)
+    new_x = x + math.cos(accel_dir) * accel_vel
+    new_z = z + math.sin(accel_dir) * accel_vel
+    return new_x, new_z
+end
+
+function move_ground(x_input, z_input)
+
+end
+
+function limit(x, max)
+    if x > max then
+        return max
+    elseif x < -max then
+        return -max
+    end
+    return x
 end
 
 function update(scene, object, delta)
@@ -41,35 +70,60 @@ function update(scene, object, delta)
     physics_set_rotation(object, 0, x_rot, 0)
     gameobject_set_rotation(camera, y_rot, 0, 0)
 
-    acceleration = gameobject_get_number(object, "acceleration")
+    friction = gameobject_get_number(object, "friction")
     max_velocity = gameobject_get_number(object, "max_velocity")
+    grounded = gameobject_get_integer(object, "grounded")
+    jump_speed = gameobject_get_number(object, "jump_speed")
+    gravity = gameobject_get_number(object, "gravity")
+
+    acceleration = 0
+    if grounded > 0 then
+        acceleration = gameobject_get_number(object, "ground_acceleration")
+    else
+        acceleration = gameobject_get_number(object, "air_acceleration")
+    end
 
     x_axis = gameobject_get_number(object, "x_axis")
     y_axis = gameobject_get_number(object, "y_axis")
     gameobject_set_number(object, "y_axis", 0)
     z_axis = gameobject_get_number(object, "z_axis")
-    wish_vel = math.sqrt(x_axis * x_axis + z_axis * z_axis)
 
-    x_cur_vel, y_cur_vel, z_cur_vel = physics_get_velocity(object)
-    cur_vel = math.sqrt(x_cur_vel * x_cur_vel + z_cur_vel * z_cur_vel)
+    cur_x, cur_y, cur_z = physics_get_velocity(object)
+    cur_y = cur_y + gravity * delta + y_axis * jump_speed
+    cur_vel = math.sqrt(cur_x * cur_x + cur_z * cur_z)
 
-    -- dot product
-    proj_vel = dot(x_axis, z_axis, x_cur_vel, z_cur_vel) --(x_axis * x_cur_vel + z_axis * z_cur_vel) / (x_axis * x_axis + z_axis * z_axis)
-    accel_vel = acceleration * delta
+    print(cur_vel)
 
-    if proj_vel + accel_vel > max_velocity then
-        accel_vel = max_velocity - proj_vel
+    input_vel = math.sqrt(x_axis * x_axis + z_axis * z_axis)
+    if input_vel > 1 then
+        x_axis = x_axis / input_vel
+        z_axis = z_axis / input_vel
+        input_vel = 1
+    end
+    x_mag = math.cos(x_rot) * x_axis - math.sin(x_rot) * z_axis
+    z_mag = math.sin(x_rot) * x_axis + math.cos(x_rot) * z_axis
+
+    wish_speed = input_vel * max_velocity
+    current_speed = dot({cur_x, cur_z}, {x_mag, z_mag})
+    add_speed = wish_speed - current_speed
+
+    if math.abs(cur_vel) > 0 and grounded > 0 then
+        drop = cur_vel * friction * delta
+        percent = math.max(cur_vel - drop, 0) / cur_vel
+        cur_x = cur_x * percent
+        cur_z = cur_z * percent
     end
 
-    x_accel = (math.cos(x_rot) * x_axis - math.sin(x_rot) * z_axis) * accel_vel
-    z_accel = (math.sin(x_rot) * x_axis + math.cos(x_rot) * z_axis) * accel_vel
-    x_vel = x_cur_vel + x_accel
-    z_vel = z_cur_vel + z_accel
+    if add_speed > 0 then
+        accel_speed = acceleration * delta * wish_speed
+        if accel_speed > add_speed then
+            accel_speed = add_speed
+        end
 
-    y_vel = y_axis * gameobject_get_number(object, "jump_speed") + y_cur_vel
-
-    physics_set_velocity(object, x_vel, y_vel, z_vel)
-    --physics_set_velocity(object, x_speed, y, z_speed)
+        physics_set_velocity(object, cur_x + accel_speed * x_mag, cur_y, cur_z + accel_speed * z_mag)
+    else
+        physics_set_velocity(object, cur_x, cur_y, cur_z)
+    end
 end
 
 function on_cursor_pos(scene, object, x, y)
@@ -98,7 +152,9 @@ function on_button_down(scene, object, button_code)
         x_axis = gameobject_get_number(object, "x_axis")
         gameobject_set_number(object, "x_axis", x_axis + 1)
     elseif button_code == "Space" then
-        gameobject_set_number(object, "y_axis", 1)
+        if gameobject_get_integer(object, "grounded") > 0 then
+            gameobject_set_number(object, "y_axis", 1)
+        end
     end
 end
 
@@ -119,9 +175,11 @@ function on_button_up(scene, object, button_code)
 end
 
 function on_collision_enter(scene, object, other)
-    print("collision enter")
+    count = gameobject_get_integer(object, "grounded")
+    gameobject_set_integer(object, "grounded", count + 1)
 end
 
 function on_collision_exit(scene, object, other)
-    print("collision exit")
+    count = gameobject_get_integer(object, "grounded")
+    gameobject_set_integer(object, "grounded", count - 1)
 end
--- scene_0.lua
+++ scene_0.lua
@@ -7,26 +7,28 @@ function setup(scene, object)
     scene_add_script(scene, player, "player.lua")
     physics_set_position(player, 0, 2, 5)
 
-    --for x = 0, 10 do
-    --    for y = 0, 10 do
-    --        cube = scene_add_gameobject(scene, "cube.dae")
-    --        gameobject_scale(cube, 1)
-    --        gameobject_transform(cube, x*2, y*2, 0)
-    --        physics_init_box(scene, cube, 10, 1, 1, 1)
-    --    end
-    --end
+    for x = 0, 10 do
+        for y = 0, 10 do
+          for i = 1, 2 do
+            cube = scene_add_gameobject(scene, "cube.dae")
+            gameobject_scale(cube, 1)
+            gameobject_transform(cube, x*1, i, y*1)
+            physics_init_box(scene, cube, 10, 0.5, 0.5, 0.5)
+          end
+        end
+    end
 
-    --cube = scene_add_gameobject(scene, "cube.dae")
-    --gameobject_transform(cube, 5, 1, 0)
-    --physics_init_box(scene, cube, 1, 1, 1, 1)
+    cube = scene_add_gameobject(scene, "cube.dae")
+    gameobject_transform(cube, 5, 1, 0)
+    physics_init_box(scene, cube, 1, 1, 1, 1)
 
     --cube = scene_add_gameobject(scene, "cube.dae")
     --gameobject_transform(cube, 0, 1, 5)
 
     plane = scene_add_gameobject(scene, "plane.dae")
     gameobject_transform(plane, 0, 0, 0)
-    gameobject_scale(plane, 1000)
-    physics_init_box(scene, plane, 0, 1000, 0.1, 1000)
+    gameobject_scale(plane, 50)
+    physics_init_box(scene, plane, 0, 50, 0.1, 50)
 
     --other2 = scene_add_gameobject(scene, "cube.dae")
     --scene_add_script(scene, other2, "test.lua")
